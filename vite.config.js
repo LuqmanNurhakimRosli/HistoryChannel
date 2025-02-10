32840f122a496527fee9c49c1abff3303a4a834c
Luqman Nurhakim <luqmanrosli907@gmail.com>
2025-02-11 01:58:35 +0800

    penAT

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-export default {
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+export default defineConfig({
+  plugins: [react()],
   build: {
     rollupOptions: {
+      external: ['react-toastify'], // Exclude react-toastify from the bundle
       output: {
         manualChunks: {
           // Split libraries into their own chunks
@@ -10,4 +15,4 @@ export default {
     },
     chunkSizeWarningLimit: 1000, // Optionally increase the warning limit
   },
-};
+});
