03136a3613dc1abe23c9b0b87b26a952764a3f9f
Luqman Nurhakim <luqmanrosli907@gmail.com>
2025-02-18 00:18:51 +0800

    done design

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -2,8 +2,8 @@
 
 const Dashboard = () => {
   return (
-    <div className=" w-xl mx-auto bg-gray-100 py-16">
-      <h1 className="text-5xl font-bold mb-4 text-gray-800">Dashboard</h1>
+    <div className="mx-auto max-w-4xl p-4 bg-gray-100 py-16">
+      <h1 className="text-5xl font-bold mb-4 text-gray-800 text-center">Dashboard</h1>
       <div className="text-lg text-gray-600 text-center">
         <p>Coming Soon!</p>
       </div>
