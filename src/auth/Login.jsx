a3e72e567faa7799adcaed776a8cbabf6f9a53d6
Luqman Nurhakim <luqmanrosli907@gmail.com>
2025-02-20 15:07:27 +0800

    wnxjgwkx

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import {
   signInWithEmailAndPassword,
+  signInWithPopup,
   getRedirectResult,
   GoogleAuthProvider,
   signInWithRedirect,
@@ -8,6 +9,7 @@ import {
   browserLocalPersistence,
   onAuthStateChanged,
 } from "firebase/auth";
+import { isMobile } from "react-device-detect";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -71,13 +73,21 @@ function Login() {
   const handleGoogleLogin = async () => {
     try {
       await setPersistence(auth, browserLocalPersistence);
-      await signInWithRedirect(auth, googleProvider); // Always use redirect method
+
+      if (isMobile) {
+        const finish = await signInWithRedirect(auth, googleProvider);
+        toast.success(`Welcome, ${finish.user.displayName}!`);
+        navigate("/dashboard");
+      } else {
+        const result = await signInWithPopup(auth, googleProvider);
+        toast.success(`Welcome, ${result.user.displayName}!`);
+        navigate("/dashboard");
+      }
     } catch (err) {
       console.error("Google Login Error:", err);
       toast.error("Google login failed!");
     }
   };
-  
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-b from-gray-700 to-gray-900">
